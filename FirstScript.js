bedc706644f165dc188b08788c7b9640f53ec64d
CostiDranca <37100776+CostiDranca@users.noreply.github.com>
2020-06-23 13:48:43 +0100

    Add files via upload

---
+++ FirstScript.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+   }
+
+function functionStartingSite() {
+    document.getElementById("backgroundAudio").play();
+}
+window.onload = functionStartingSite;

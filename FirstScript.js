39262c9ca0e095dfc9bd8738ed1aaef5629684da
CostiDranca <37100776+CostiDranca@users.noreply.github.com>
2020-06-24 22:44:56 +0100

    Add files via upload

--- FirstScript.js
+++ FirstScript.js
@@ -6,3 +6,13 @@ function functionStartingSite() {
     document.getElementById("backgroundAudio").play();
 }
 window.onload = functionStartingSite;
+
+function changeContent(cod) {
+    var contents = document.getElementsByClassName("content");
+    var l = contents.length;
+    for(i = 0; i < l; i++)
+    {
+        contents[i].style.visibility = 'hidden';
+    }
+    contents[cod].style.visibility = 'visible';
+}
